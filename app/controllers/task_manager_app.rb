1ef950890f8116e2ea458c34a799129eaad8e1ea
Ruth Brand <ruthbrandsoprano@gmail.com>
2019-01-21 20:36:29 -0700

    wip

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/task.rb'
 
 class TaskManangerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -32,4 +33,13 @@ class TaskManangerApp < Sinatra::Base
     erb :edit
   end
 
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
+  end
 end
